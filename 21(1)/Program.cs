f659402f04368ea54553a1b88c70367abbf5ca39
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-05-07 22:23:33 +0300

    Add files via upload

---
+++ 21(1)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_1_
+{
+    class Program
+    {
+        delegate void Show();
+        static void Main(string[] args)
+        {
+            Show show;
+            if(DateTime.Now.Hour < 12)
+            {
+                show = GoodMorning;
+            }
+            else
+            {
+                show = GoodEvening;
+            }
+            show.Invoke();
+            Console.ReadLine();
+        }
+
+        private static void GoodMorning()
+        {
+            Console.WriteLine("Доброу утро!");
+        }
+        private static void GoodEvening()
+        {
+            Console.WriteLine("Добрый вечер!");
+        }
+    }
+}
---
+++ 21(2)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_2_
+{
+    class Program
+    {
+        delegate int Func(int a, int b);
+        static void Main(string[] args)
+        {
+            Func func = new Func(Add);
+            int res = func.Invoke(2, 3);
+            Console.WriteLine(res);
+            func = Sub;
+            res = func.Invoke(2, 3);
+            Console.WriteLine(res);
+            func = Mul;
+            res = func.Invoke(2, 3);
+            Console.WriteLine(res);
+            func = Div;
+            res = func.Invoke(3, 3);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+        static int Add(int x,int y)
+        {
+            return x + y;
+        }
+        static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        static int Mul(int x, int y)
+        {
+            return x * y;
+        }
+        static int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+    }
+}
